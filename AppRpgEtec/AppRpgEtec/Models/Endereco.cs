798a063385869680e6ff976c89ee449249c917f2
unknown <limajeffersonn654@gmail.com>
2025-09-22 08:56:35 -0300

    prova

---
+++ AppRpgEtec/AppRpgEtec/Models/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRpgEtec.Models
+{
+    public class Endereco
+    {
+        public int Place_Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Lat { get; set; } = string.Empty;
+        public string Lon { get; set; } = string.Empty;
+        public string Display_Nome { get; set; } = string.Empty;
+    }
+}
---
+++ AppRpgEtec/AppRpgEtec/Services/Enderecos/EnderecoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+
+namespace AppRpgEtec.Services.Enderecos
+{
+    public class EnderecoService
+    {
+        private readonly Request _request;
+        private const string apiUrlBase = "https://nominatim.openstreetmap.org/search?format=json&q=";
+        public async Task<Endereco> GetEnderecoAsync(string cep)
+        {
+            string urlComplementar = string.Format("{lat}","{lon}");
+            var personagem = await _request.GetAsync<Models.Endereco>(apiUrlBase+urlComplementar, string.Empty);
+            return personagem;
+        }
+    }
+}
